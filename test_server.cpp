f64296b4542022265591a755597ebad2a57d26bb
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-21 09:45:13 +0500

    Update test_server.cpp

--- test_server.cpp
+++ test_server.cpp
@@ -42,6 +42,10 @@ protected:
       ipOfServer.sin_family = AF_INET;
       ipOfServer.sin_port = htons(2017);
       ipOfServer.sin_addr.s_addr = inet_addr("127.0.0.1");
+      printf("\n");
+      std::cout << ipOfServer.sin_family << std::endl;
+      std::cout << ipOfServer.sin_port << std::endl;
+      std::cout << ipOfServer.sin_addr.s_addr << std::endl;
       if(connect(CreateSocket, (struct sockaddr *)&ipOfServer, sizeof(ipOfServer))<0)
           printf("Connection failed due to port and ip problems\n");
       else
